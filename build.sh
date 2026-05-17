77e13283ab897ce300e5fb293a20844abece444c
SRIPADA DHEERAJ <sripadadheeraj2025@gmail.com>
2026-05-17 12:26:04 +0530

    Remove superuser creation from build script

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='Dheeraj').exists() or User.objects.create_superuser('Dheeraj', '', 'Projectmark1')" | python manage.py shell
