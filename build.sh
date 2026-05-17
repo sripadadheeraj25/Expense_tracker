54254e58e83ab437e7013a841a09e4f1dd5b097e
SRIPADA DHEERAJ <sripadadheeraj2025@gmail.com>
2026-05-17 12:13:09 +0530

    Auto create superuser on deploy

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='dheeraj').exists() or User.objects.create_superuser('Dheeraj', '', 'Projectmark1')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='Dheeraj').exists() or User.objects.create_superuser('Dheeraj', '', 'Projectmark1')" | python manage.py shell
